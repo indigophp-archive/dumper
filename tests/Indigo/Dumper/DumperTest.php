7ff5f896324cf53c6c6f4ead157b0a17f43dbf69
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-01-03 18:50:51 +0100

    Added dumper test

---
+++ tests/Indigo/Dumper/DumperTest.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * This file is part of the Indigo Dumper package.
+ *
+ * (c) IndigoPHP Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Dumper;
+
+use Indigo\Dumper\Connector\SqliteConnector;
+use Indigo\Dumper\Store\VariableStore;
+
+/**
+ * Dumper Test
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class DumperTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Dumper object
+     *
+     * @var Dumper
+     */
+    protected $dumper;
+
+    public function setUp()
+    {
+        $options = array(
+            'database' => __DIR__ . '/../../test.sqlite'
+        );
+
+        $connector = new SqliteConnector($options);
+        $store = new VariableStore;
+
+        $this->dumper = new Dumper($connector, $store);
+    }
+
+    public function testSetStore()
+    {
+        $this->assertInstanceOf(
+            'Indigo\\Dumper\\Dumper',
+            $this->dumper->setStore(new VariableStore)
+        );
+    }
+
+    public function testDump()
+    {
+        $dump = $this->dumper->dump();
+
+        $this->assertTrue(is_bool($dump));
+    }
+
+    public function testOption()
+    {
+        $this->assertEquals(null, $this->dumper->getOption('nothing_here'));
+        $this->assertEquals(null, $this->dumper->getConnectorOption('nothing_here'));
+    }
+
+    public function testIncludeTable()
+    {
+        $this->assertFalse($this->dumper->hasTable());
+
+        $this->assertInstanceOf(
+            'Indigo\\Dumper\\Dumper',
+            $this->dumper->excludeTable('test2')
+        );
+
+        $this->assertFalse($this->dumper->hasTable());
+        $this->assertTrue($this->dumper->isTableExcluded('test2'));
+
+        $this->assertInstanceOf(
+            'Indigo\\Dumper\\Dumper',
+            $this->dumper->includeTable('test')
+        );
+
+        $this->assertTrue($this->dumper->isTableIncluded('test'));
+
+        $this->assertTrue($this->dumper->hasTable());
+    }
+
+    public function testIncludeView()
+    {
+        $this->assertFalse($this->dumper->hasView());
+
+        $this->assertInstanceOf(
+            'Indigo\\Dumper\\Dumper',
+            $this->dumper->excludeView('v_test2')
+        );
+
+        $this->assertFalse($this->dumper->hasView());
+        $this->assertTrue($this->dumper->isViewExcluded('v_test2'));
+
+        $this->assertInstanceOf(
+            'Indigo\\Dumper\\Dumper',
+            $this->dumper->includeView('v_test')
+        );
+
+        $this->assertTrue($this->dumper->isViewIncluded('v_test'));
+
+        $this->assertTrue($this->dumper->hasView());
+    }
+}
